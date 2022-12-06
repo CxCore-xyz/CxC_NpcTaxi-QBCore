b57935a1b0990742044ad25b395bcfd4d39c2db1
CxCore.xyz <91425802+CxCore-xyz@users.noreply.github.com>
2022-12-06 18:57:30 +0200

    +Debug Integration For Pay Function
    +Debug Integration For Pay Function

--- CxC_NpcTaxi/server/main.lua
+++ CxC_NpcTaxi/server/main.lua
@@ -6,4 +6,5 @@ AddEventHandler('CxC:NPCTaxi:pay', function(price)
      local xPlayer = QBCore.Functions.GetPlayer(_source) 
      xPlayer.Functions.RemoveMoney(Config.PaymentType, price)
      TriggerClientEvent('QBCore:Notify', source, 'You paid total of $'..price..' for the taxi journey!', "primary")
+     if Config.Debug then print("^0[^5Debug^7][^3Information^0]: ^2Successfully removed money for taxi ride from this Player-ID: ".._source.." ^3Amount: $"..price) end     
 end)
